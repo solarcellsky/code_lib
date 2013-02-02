dcefc92a478d642e05bab510dbbd23363401454e
solar <solarcellsky@gmail.com>
2013-02-02 18:48:37 +0800

    .

--- bgIntime/app.js
+++ bgIntime/app.js
@@ -1,6 +1,11 @@
-var app = {};
-app.show_different_bg = function(){
+//colorEgg module
+(function(colorEgg) {
     var D = new Date();
-    var currentHour = D.getHours();
-    jQuery('#page_background').append('<img src=bg/bg_'+parseInt(currentHour/3)+'.jpg width=100% height=100%>');
-}
+    colorEgg.show_different_bg = function(){
+        var currentHour = D.getHours();
+        jQuery('#page_background').append('<img src=bg/bg_'+parseInt(currentHour/3)+'.jpg width=100% height=100%>');
+    };
+    colorEgg.show_currentDate = function(){
+        jQuery('time').append(D.toString());
+    };
+})(window.colorEgg = window.colorEgg || {});
