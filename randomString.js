50741ec973aa052a9d99312b89ba0c3ed936bc5a
solar <solarcellsky@gmail.com>
2013-02-02 12:52:26 +0800

    javascript generate a random string

---
+++ randomString.js
@@ -0,0 +1,12 @@
+function randomString(len, charSet) {
+    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var randomString = '';
+    for (var i = 0; i < len; i++) {
+        var randomPoz = Math.floor(Math.random() * charSet.length);
+        randomString += charSet.substring(randomPoz,randomPoz+1);
+    }
+    return randomString;
+}
+var randomRecordName = randomString(16);
+
+console.log(randomRecordName);
