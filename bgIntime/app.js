ccb5c114b498f2e7256483ff588626c1189b28e2
solar <solarcellsky@gmail.com>
2013-02-02 18:04:01 +0800

    .

---
+++ bgIntime/app.js
@@ -0,0 +1,6 @@
+var app = {};
+app.show_different_bg = function(){
+    var D = new Date();
+    var currentHour = D.getHours();
+    $('#page_background').append('<img src=bg/bg_'+parseInt(currentHour/3)+'.jpg width=100% height=100%>');
+}
